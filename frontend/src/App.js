20fdb97f26153f77a539b877f0197b2b0396a61c
Bruno Gonçalves <brgoncalves@hotmail.com.br>
2019-10-16 09:12:49 -0300

    teste

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,25 @@
 import React from 'react';
 import './App.css';
 
+import logo from './assets/logo.svg';
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className="container">
+      <img src={logo} alt="AirCnC"/>
+
+      <div className="content">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          texto
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+        <form>
+          <label htmlFor="email*"E-MAIL ></label>
+        </form>
+      </div>
     </div>
   );
 }
 
 export default App;
+ 
